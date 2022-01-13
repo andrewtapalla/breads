932746ae5d26e689e6900fdc52f99eb510a9e42a
Andrew Tapalla <tapalla_andrew@yahoo.com>
2022-01-12 20:10:32 -0800

    sending index and show data

--- server.js
+++ server.js
@@ -13,6 +13,11 @@ app.get('/', (req, res) => {
 const breadsController = require('./controllers/breads_controller.js')
 app.use('/breads', breadsController)
 
+// MIDDLEWARE
+app.set('views', __dirname + '/views')
+app.set('view engine', 'jsx')
+app.engine('jsx', require('express-react-views').createEngine())
+
 app.listen(PORT, () => {
     console.log('nomming at port', PORT);
 })

a2c0fa6140554f6875d6b66f9e6a68a533a862ea
Andrew Tapalla <tapalla_andrew@yahoo.com>
2022-01-12 19:06:40 -0800

    sending index and show data

---
+++ server.js
@@ -0,0 +1,18 @@
+const express = require('express')
+
+require('dotenv').config()
+const PORT = process.env.PORT
+const app = express()
+
+// ROUTES
+app.get('/', (req, res) => {
+    res.send('Welcome to an Awesome App about Breads!')
+})
+
+// BREADS
+const breadsController = require('./controllers/breads_controller.js')
+app.use('/breads', breadsController)
+
+app.listen(PORT, () => {
+    console.log('nomming at port', PORT);
+})

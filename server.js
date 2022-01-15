a28bcf237de3ab425b76d3b1ef40b30ee0cde292
Andrew Tapalla <tapalla_andrew@yahoo.com>
2022-01-15 10:02:53 -0800

    sending index and show data

--- server.js
+++ server.js
@@ -4,6 +4,12 @@ require('dotenv').config()
 const PORT = process.env.PORT
 const app = express()
 
+// MIDDLEWARE
+app.use(express.static('public'))
+app.set('views', __dirname + '/views')
+app.set('view engine', 'jsx')
+app.engine('jsx', require('express-react-views').createEngine())
+
 // ROUTES
 app.get('/', (req, res) => {
     res.send('Welcome to an Awesome App about Breads!')
@@ -13,10 +19,10 @@ app.get('/', (req, res) => {
 const breadsController = require('./controllers/breads_controller.js')
 app.use('/breads', breadsController)
 
-// MIDDLEWARE
-app.set('views', __dirname + '/views')
-app.set('view engine', 'jsx')
-app.engine('jsx', require('express-react-views').createEngine())
+// 404 Page
+app.get('*', (req, res) => {
+    res.send('404')
+  })
 
 app.listen(PORT, () => {
     console.log('nomming at port', PORT);
